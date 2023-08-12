40862863db371904d58252195dfac36caaaa0c9a
Jeremy Cope <jeremy.cope7142@gmail.com>
2023-08-12 11:00:10 -0700

    inital commit

---
+++ include/main.h
@@ -0,0 +1,116 @@
+/**
+ * \file main.h
+ *
+ * Contains common definitions and header files used throughout your PROS
+ * project.
+ *
+ * \copyright Copyright (c) 2017-2023, Purdue University ACM SIGBots.
+ * All rights reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef _PROS_MAIN_H_
+#define _PROS_MAIN_H_
+
+/**
+ * If defined, some commonly used enums will have preprocessor macros which give
+ * a shorter, more convenient naming pattern. If this isn't desired, simply
+ * comment the following line out.
+ *
+ * For instance, E_CONTROLLER_MASTER has a shorter name: CONTROLLER_MASTER.
+ * E_CONTROLLER_MASTER is pedantically correct within the PROS styleguide, but
+ * not convenient for most student programmers.
+ */
+#define PROS_USE_SIMPLE_NAMES
+
+/**
+ * If defined, C++ literals will be available for use. All literals are in the
+ * pros::literals namespace.
+ *
+ * For instance, you can do `4_mtr = 50` to set motor 4's target velocity to 50
+ */
+#define PROS_USE_LITERALS
+
+#include "api.h"
+
+/**
+ * You should add more #includes here
+ */
+//#include "okapi/api.hpp"
+//#include "pros/api_legacy.h"
+
+/**
+ * If you find doing pros::Motor() to be tedious and you'd prefer just to do
+ * Motor, you can use the namespace with the following commented out line.
+ *
+ * IMPORTANT: Only the okapi or pros namespace may be used, not both
+ * concurrently! The okapi namespace will export all symbols inside the pros
+ * namespace.
+ */
+using namespace pros;
+using namespace pros::literals;
+// using namespace okapi;
+
+/**
+ * Prototypes for the competition control tasks are redefined here to ensure
+ * that they can be called from user code (i.e. calling autonomous from a
+ * button press in opcontrol() for testing purposes).
+ */
+
+class Robot {
+    public:
+        Motor frontLeft;
+        Motor frontRight;
+        Motor backLeft;
+        Motor backRight;
+
+        Robot(int* motors) : 
+            frontLeft (*motors),
+            frontRight (*(motors + 1)),
+            backLeft (*(motors + 2)),
+            backRight (*(motors + 3))
+        {
+
+        }
+
+
+    void driveGo(Controller controller)
+    {
+	    int leftStick = controller.get_analog(E_CONTROLLER_ANALOG_LEFT_Y);
+	    int rightStick = controller.get_analog(E_CONTROLLER_ANALOG_RIGHT_Y);
+
+        this->frontLeft.move(leftStick);
+        this->backLeft.move(leftStick);
+
+        this->frontRight.move(rightStick);
+        this->backRight.move(rightStick);
+    }
+};
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+void autonomous(void);
+void initialize(void);
+void disabled(void);
+void competition_initialize(void);
+void opcontrol(void);
+#ifdef __cplusplus
+}
+#endif
+
+#ifdef __cplusplus
+/**
+ * You can add C++-only headers here
+ */
+//#include <iostream>
+#endif
+
+// global variables
+int motors[] = {1,11,2,12};
+Robot robot (motors);
+
+#endif  // _PROS_MAIN_H_
