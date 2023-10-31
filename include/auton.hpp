ca32a6193407857c5dcf6e3cfbc2e5ef2722d009
jeremy <jcope0787@protonmail.com>
2023-10-30 18:07:37 -0700

    added functions

--- include/auton.hpp
+++ include/auton.hpp
@@ -1,6 +1,10 @@
 #ifndef AUTON_HEADERPP_OVERUNDER
 #define AUTON_HEADERPP_OVERUNDER
 
-#include "Robot.hpp"
+#include "main.hpp"
 
+namespace auton
+{
+void push();
+}
 #endif //AUTON_HEADERPP_OVERUNDER
--- src/auton.cpp
+++ src/auton.cpp
@@ -1,2 +1,6 @@
 #include "../include/auton.hpp"
 
+void auton::push()
+{
+}
+
